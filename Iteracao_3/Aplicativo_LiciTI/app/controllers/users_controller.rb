07a150a4a92539baca5d22a8c341f10affb9c130
Elverson <esm6@cin.ufpe.br>
2016-11-10 19:22:51 -0300

    Ajustes layout

--- Iteracao_3/Aplicativo_LiciTI/app/controllers/users_controller.rb
+++ Iteracao_3/Aplicativo_LiciTI/app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 	def create 
 		@user = User.new(user_params) 
 		if @user.save 
-			session[:user_id] = @user.id 
+			session[:user_id] = @user.id
+			session[:user_id] = nil
 			redirect_to '/' 
 		else 
 			redirect_to '/signup' 
